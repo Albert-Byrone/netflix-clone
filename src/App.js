a6488a100506f23d0892dbf7397cf6fbfd8e3e34
albertbyrone <albertbyrone16777@gmail.com>
2020-12-17 10:10:14 +0300

    add row component with three props

--- src/App.js
+++ src/App.js
@@ -4,6 +4,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <Row  
+      title="PLAYING NOW"
+      fetchUrl={requests.fetchNowPlayingMovies}
+      isLargeRow
+      />
       <Row  title="Trending" fetchUrl={ requests.fetchTrending}/>
       <Row  title="Upcoming Movies" fetchUrl={ requests.fetchUpcomingMovies}/>
       <Row  title="Top Rated" fetchUrl={ requests.fetchTopRated}/>
