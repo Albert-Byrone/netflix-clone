dea21b6f5c5a13e2a54a422f591a8b0de2056d2d
albertbyrone <albertbyrone16777@gmail.com>
2020-12-17 10:09:27 +0300

    add row components that takes two props

--- src/App.js
+++ src/App.js
@@ -4,20 +4,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Row  title="Trending" fetchUrl={ requests.fetchTrending}/>
+      <Row  title="Upcoming Movies" fetchUrl={ requests.fetchUpcomingMovies}/>
+      <Row  title="Top Rated" fetchUrl={ requests.fetchTopRated}/>
+      <Row  title="Popular Movies" fetchUrl={ requests.fetchPopolarMovies}/>
+
     </div>
   );
 }
